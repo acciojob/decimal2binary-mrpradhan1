e84b5d38987433e2a798dae7aa598bb2cc3eb2f8
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-07-27 16:22:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-function decimalToBinary(num) {
-  //Write you code here
-	let str ="";
-	while(num>0){
-		let rem= num%2;
-		str +=rem;
-		num=Math.floor(num/2);
-	}
-  console.log(decimalToBinary(7));
-console.log(decimalToBinary(10)); 
-console.log(decimalToBinary(33));
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-
-window.decimalToBinary = decimalToBinary;
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 

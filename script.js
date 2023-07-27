5bb559165ef0bce437733fef683a5fd9b5cd4636
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-07-27 16:13:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
   //Write you code here
-  
+	let str ="";
+	while(num>0){
+		let rem= num%2;
+		str +=rem;
+		num=Math.floor(num/2);
+	}
+  console.log(str);
 }
 
 window.decimalToBinary = decimalToBinary;
